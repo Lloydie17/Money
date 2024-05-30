76d61920b1d33cc33abbfff39c10f6d1618606f8
unknown <YY122@ccs.local>
2024-05-30 11:02:50 +0800

    adoption-main

---
+++ adoption-main/src/app/_services/request.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '@environments/environment';
+
+const baseUrl = `${environment.apiUrl}/request`;
+
+@Injectable({ providedIn: 'root' })
+export class RequestService {
+  constructor(private http: HttpClient) { }
+
+  createRequest(petId: number): Observable<any> {
+    return this.http.post<any>(`${baseUrl}`, { petId });
+  }
+
+  getAllRequests(): Observable<any[]> {
+    return this.http.get<any[]>(`${baseUrl}/admin`);
+  }
+
+  getRequests(): Observable<any[]> {
+    return this.http.get<any[]>(`${baseUrl}`);
+  }
+
+  getUserRequests(accountId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${baseUrl}/user/${accountId}`);
+  }
+
+  deleteRequest(id: number): Observable<any> {
+    return this.http.delete<any>(`${baseUrl}/${id}`);
+  }
+}
---
+++ adoption-main/src/app/admin/admin-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { SubNavComponent } from './subnav.component';
+import { LayoutComponent } from './layout.component';
+import { OverviewComponent } from './overview.component';
+
+const accountsModule = () => import('./accounts/accounts.module').then(x => x.AccountsModule);
+const petsModule = () => import('./pets/pets.module').then(x => x.PetsModule);
+
+const routes: Routes = [
+    { path: '', component: SubNavComponent, outlet: 'subnav' },
+    {
+        path: '', component: LayoutComponent,
+        children: [
+            { path: '', component: OverviewComponent },
+            { path: 'accounts', loadChildren: accountsModule },
+            { path: 'pets', loadChildren: petsModule },
+            
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AdminRoutingModule { }
